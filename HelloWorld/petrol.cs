8591eb8c032c6cce46a11e9b576cac2da2118287
haroon <haroon@DESKTOP-792H4GJ>
2020-07-26 13:44:20 +0500

    updates

---
+++ HelloWorld/petrol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWorld
+{
+    class petrol
+    {
+        static public SqlConnection Connect()
+        {
+
+            try
+            {
+                SqlConnection thisConnection = new SqlConnection(@"Data Source=DESKTOP-792H4GJ\SQLEXPRESS;Initial Catalog=FuelPumpDB;Integrated Security=True"); thisConnection.Open();
+                return thisConnection;
+            }
+            catch
+            {
+                MessageBox.Show("Database Connection Error");
+                return null;
+            }
+
+        }
+
+    }
+}
